5006d8bba44240de5f5fe4fe4d56d8b93927b1bf
Ross Amiri <aa.smpro@gmail.com>
2024-12-30 16:30:31 +0100

    feat: adding suspense

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline"
+
 export default function Page() {
-    return <p>Invoices Page</p>
+    return <p>Invoices Page <ArrowPathIcon className="w-[24px] h-[24px] inline-block animate-spin" /></p>
 }
