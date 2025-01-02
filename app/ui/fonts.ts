537285b9931bd0d344a24f18617a155303e7c0d6
Ross Amiri <aa.smpro@gmail.com>
2025-01-02 16:39:24 +0100

    feat: adding ppr

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, JetBrains_Mono } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({subsets: ['latin'], weight: ["400", "700"]})
+export const jbm = JetBrains_Mono({subsets: ['latin']})
